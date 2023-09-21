dbd6b15b47c27d160d7107e6b4519a09da4d1695
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-09-21 18:08:39 -0400

    GET product by id.

--- src/main/java/com/mywebsite/ecommerce/controllers/ProductController.java
+++ src/main/java/com/mywebsite/ecommerce/controllers/ProductController.java
@@ -1,26 +1,23 @@
 package com.mywebsite.ecommerce.controllers;
 
-import com.mywebsite.ecommerce.entities.Product;
-import com.mywebsite.ecommerce.repositories.ProductRepository;
+import com.mywebsite.ecommerce.dto.ProductDTO;
+import com.mywebsite.ecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Optional;
 
 
-@RestController // essas duas annotations vão configurar essa classe para que ela responda via web.
-@RequestMapping(value = "/products")// vai responder pela rota "/products"
+@RestController
+@RequestMapping(value = "/products")
 public class ProductController {
 
     @Autowired
-    private ProductRepository repository;
+    private ProductService service;
 
-    @GetMapping
-    public String test() {
-        Optional<Product> result = repository.findById(2L);  // esse 'result' é o resultado da consulta no banco de dados
-        Product product = result.get(); // esse '.get' pega o objeto que esta dentro do Optional, e estamos passando esse resultado para um objeto 'product'
-        return product.getName();
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return service.findById(id);
     }
 }
---
+++ src/main/java/com/mywebsite/ecommerce/services/ProductService.java
@@ -0,0 +1,21 @@
+package com.mywebsite.ecommerce.services;
+
+import com.mywebsite.ecommerce.dto.ProductDTO;
+import com.mywebsite.ecommerce.entities.Product;
+import com.mywebsite.ecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+    }
+}
