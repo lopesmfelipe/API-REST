8b3595579f8f3ed65b8db38692f8f8313e55e0d9
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-11-14 00:52:22 -0400

    Update

--- src/main/java/com/mywebsite/ecommerce/controllers/ProductController.java
+++ src/main/java/com/mywebsite/ecommerce/controllers/ProductController.java
@@ -26,8 +26,10 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
-        Page<ProductDTO> dto = service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(
+            @RequestParam(name = "name", defaultValue = "") String name,
+            Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
