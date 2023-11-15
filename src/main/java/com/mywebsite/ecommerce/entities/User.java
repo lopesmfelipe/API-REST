109bda4acd9561edfd362e593df76c3fd02c90c5
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-11-14 20:02:55 -0400

    UserDetailsService

--- src/main/java/com/mywebsite/ecommerce/entities/User.java
+++ src/main/java/com/mywebsite/ecommerce/entities/User.java
@@ -131,7 +131,7 @@ public class User implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
     }
-    
+
     @Override
     public String getUsername() {
         return email;
