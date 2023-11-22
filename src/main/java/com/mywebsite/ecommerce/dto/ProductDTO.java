c70a8b66cef2f0fe2845f4f1b560957a9c289b50
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-11-22 09:07:55 -0400

    Fix validation

--- src/main/java/com/mywebsite/ecommerce/dto/ProductDTO.java
+++ src/main/java/com/mywebsite/ecommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.mywebsite.ecommerce.dto;
 
 import com.mywebsite.ecommerce.entities.Category;
 import com.mywebsite.ecommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +16,7 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo obrigatório")
     private String description;
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
